e7f521f3d5eb5b3d65db66860d0ea01d1fb4e988
ChandruMoger <chandrumoger999@gmail.com>
2020-02-07 10:02:31 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.port || 4000;
+
+app.get('/', (req, res)=>{ 
+    res.send("Hello chandru from degital oceal");
+})
+
+app.listen(port, () => {
+    console.log("Server up and running on port" + port)
+})
