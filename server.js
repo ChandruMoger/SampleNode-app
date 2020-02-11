0ba1b722c31431c4c49562b280ae58f61e1e35ee
ChandruMoger <chandrumoger999@gmail.com>
2020-02-11 08:38:13 +0000

    Changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.port || 4000;
 
 app.get('/', (req, res)=>{ 
-    res.send("Hello chandru from degital oceal");
+    res.send("Hello chandru from degital ocean");
 })
 
 app.listen(port, () => {
